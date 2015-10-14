1db3ef5dce2929b687d84d4975049e9128e51a26
IvanCoronado <ivacormor@gmail.com>
2015-10-14 21:31:35 +0200

    ADD markers with css

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -169,27 +169,105 @@
             zoom: 14
         };
         $scope.options = {
-            /* mapTypeId: google.maps.MapTypeId.ROADMAP,                   
-             mapTypeControl: false,
-             mapTypeControlOptions: {
-                 style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-                 position: google.maps.ControlPosition.BOTTOM_CENTER
-             },*/
-            minZoom: 14,
-            maxZoom: 14,
             draggable: false,
-            /* panControlOptions: {
-                 position: google.maps.ControlPosition.RIGHT_BOTTOM
-             },*/
+            fit:true,
+            minZoom: 13,
+            maxZoom: 14,
             disableDefaultUI: true,
-            /*panControl: true,
-            zoomControl: true,
-            scaleControl: false,
-            streetViewControl: false,
-            overviewMapControl: false,*/
             styles: blackMap
         };
 
+        $scope.randomMarkers = [{
+            id: 0,
+            latitude: 41.374565,
+            longitude: 2.189239,
+            title: "m0"
+        }];
+
+        //http://google-maps-utility-library-v3.googlecode.com/svn/tags/markerwithlabel/1.1.9/docs/reference.html
+        $scope.markers = [{
+            id: 101,
+            latitude: 41.374565,
+            longitude: 2.185239,
+            icon: "false",
+            labelContent: "•••",
+            /*
+             *  The name of the CSS class defining the styles for the label. 
+             *  Note that style values for position, overflow, top, left, zIndex, display, marginLeft, and marginTop are ignored; 
+             *  these styles are for internal use only.
+             **/
+            labelClass: "marker",
+            /*
+             *  An object literal whose properties define specific CSS style values to be applied to the label. 
+             *  Style values defined here override those that may be defined in the labelClass style sheet. 
+             *  If this property is changed after the label has been created, all previously set styles (except those defined in the style sheet) are removed from the label before the new style values are applied.
+             **/
+            labelStyle: {
+                width: (30+(3*5))+"px",
+                height: (30+(3*5))+"px",
+                lineHeight: (30+(3*5))+"px"
+            }
+        }, {
+            id: 102,
+            latitude: 41.378565,
+            longitude: 2.15,
+            icon: "false",
+            labelContent: "•",
+            labelClass: "marker",
+            labelStyle: {
+                width: (30+(1*5))+"px",
+                height: (30+(1*5))+"px",
+                lineHeight: (30+(1*5))+"px"
+            }
+        }, {
+            id: 103,
+            latitude: 41.387965,
+            longitude: 2.156,
+            icon: "false",
+            labelContent: "•",
+            labelClass: "marker",
+            labelStyle: {
+                width: (30+(1*5))+"px",
+                height: (30+(1*5))+"px",
+                lineHeight: (30+(1*5))+"px"
+            }
+        }, {
+            id: 104,
+            latitude: 41.382965,
+            longitude: 2.16750,
+            icon: "false",
+            labelContent: "•",
+            labelClass: "marker",
+            labelStyle: {
+                width: (30+(1*5))+"px",
+                height: (30+(1*5))+"px",
+                lineHeight: (30+(1*5))+"px"
+            }
+        }, {
+            id: 105,
+            latitude: 41.373,
+            longitude: 2.16,
+            icon: "false",
+            labelContent: "•",
+            labelClass: "marker",
+            labelStyle: {
+                width: (30+(1*5))+"px",
+                height: (30+(1*5))+"px",
+                lineHeight: (30+(1*5))+"px"
+            }
+        }, {
+            id: 106,
+            latitude: 41.375965,
+            longitude: 2.17400,
+            icon: "false",
+            labelContent: "•",
+            labelClass: "marker",
+            labelStyle: {
+                width: (30+(1*5))+"px",
+                height: (30+(1*5))+"px",
+                lineHeight: (30+(1*5))+"px"
+            }
+        }];
         uiGmapGoogleMapApi.then(function(maps) {
 
         });
